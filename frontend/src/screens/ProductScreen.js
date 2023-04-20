f80d5580f3791557b1044bb8fd25bafe9b7ef5c3
Eli Amuyev <eli9914@gmail.com>
2023-04-20 16:37:34 +0300

    changes with backend start

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React,{useState,useEffect}  from 'react'
 import { Link,useParams} from 'react-router-dom'
 import {Row , Col , Image , ListGroup , Card , Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
-
+import axios from 'axios'
 
 
 const ProductScreen = () => {
-  const {id}= useParams();
-  const product = products.find(p => p._id === (id))
+
+const [product, setProduct]= useState({})
+const {id}= useParams();
+
+useEffect(()=>{
+
+  const fetchProduct=async()=>{
+    const res= await axios.get(`/api/product/${id}`)
+    setProduct(res.data)
+  }
+  fetchProduct()
+
+},[id])
+
+
+
+// fetching from frontend
+// const {id}= useParams();
+// const product = products.find(p => p._id === (id))
+
+
+
   return(
     <>
    <Link className='btn btn-dark my-3'> Go Back</Link>
