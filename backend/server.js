2dec4b29c624a659a6fb880725df0986ec24515e
eli9914 <eli9914@gmail.com>
2023-05-21 11:30:25 +0300

    fetching products from database

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,25 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import products from './data/products.js'
 import colors from 'colors'
+import protuctRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
 connectDB()
 
-const app=express()
+const app = express()
 
-
-app.get('/', (req,res)=>{
-    res.send('API is running...')
+app.get('/', (req, res) => {
+  res.send('API is running...')
 })
+app.use('/api/products', protuctRoutes)
 
-app.get('/api/products', (req,res)=>{
-    res.json(products)
-})
+const PORT = process.env.PORT || 5000
 
-app.get('/api/product/:id', (req,res)=>{
-    const product= products.find((p)=>p._id===req.params.id)
-    res.json(product)
-})
-
-const PORT=process.env.PORT || 5000
-
-app.listen(PORT,console.log(`'Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold))
+app.listen(
+  PORT,
+  console.log(
+    `'Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold
+  )
+)
